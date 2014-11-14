82bde9d14bd9637b01b204635fe242080e866345
WirelessCaveman <wireless.caveman@gmail.com>
2014-11-14 01:29:22 -0800

    LinkedList

--- LinkedLists/src/LinkedLists.cpp
+++ LinkedLists/src/LinkedLists.cpp
@@ -29,21 +29,6 @@ void PrintLinkedList(struct Node* head)
 	printf("\n");
 }
 
-
-//recursive
-//Node MergeLists(Node list1, Node list2) {
-//  if (list1 == null) return list2;
-//  if (list2 == null) return list1;
-//
-//  if (list1.data < list2.data) {
-//    list1.next = MergeLists(list1.next, list2);
-//    return list1;
-//  } else {
-//    list2.next = MergeLists(list2.next, list1);
-//    return list2;
-//  }
-//}
-
 struct Node* MergeLinkedLists(struct Node *l1, struct Node *l2)
 {
 	struct Node temp;
